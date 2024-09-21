9e76d5aaec896b43bba7874996f4f491e93cf928
Нина Павлова <samaraivan2005@yandex.ru>
2024-09-21 03:08:02 +0300

    task 1 nd

--- lab 2 task1/Program.cs
+++ lab 2 task1/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class ClassRoom
+{
+
+}
+
+class Pupil
+{
+    virtual public void Study()
+    {
+        Console.WriteLine("He studies");
+    }
+
+    virtual public void Read() {
+        Console.WriteLine("He reads")
+    }
+
+    public void Write() { 
+    }
+
+    public void Relax() { 
+    }
+
+}
+
+class ExcelentPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("He studies perfectly");
+    }
+
+    public override void Read()
+    {
+
+    }
+}
+
+class GoodPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("He studies normally");
+    }
+}
+
+class BadPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("He studies badly");
+    }
+}
