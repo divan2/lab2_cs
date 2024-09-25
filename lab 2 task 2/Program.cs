706a9047b2056593a51f89a11e0a4eb37fe5388c
Нина Павлова <samaraivan2005@yandex.ru>
2024-09-26 01:22:51 +0300

    laba 2 task2

---
+++ lab 2 task 2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+class Vehicle
+{
+    private double[] coordinates = new double[2];
+    protected double price;
+    protected double speed;
+    protected int year;
+
+    public Vehicle(double price, double speed, int year, params double[] coord)
+    {
+        coordinates = coord;
+        this.price = price;
+        this.speed = speed;
+        this.year = year;  
+    }
+    public void print()
+    {
+        Console.WriteLine("координаты:");
+        foreach (var v in coordinates) Console.WriteLine(v);
+
+        Console.WriteLine("price = " + price + ", speed = " + speed + ", year = " + year);
+    }
+} 
+
+class Plane : Vehicle
+{
+    protected double hight;
+    protected int pass;
+
+    public Plane(double price, double speed, int year, int pass, params double[] coord) : base(price, speed, year, coord)
+    { 
+        this.hight = coord[2];
+        this.pass = pass;
+    }
+
+    public new void print()
+    {
+        Console.WriteLine("Plane:")
+        base.print();
+        Console.WriteLine("passengers: " + pass);
+    }
+
+}
+
+class Ship : Vehicle
+{
+    protected string port;
+    protected int pass;
+
+    public Ship(double price, double speed, int year, int pass, string port, params double[] coord) : base(price, speed, year, coord)
+    {
+        this.port = port;
+        this.pass = pass;
+    }
+
+    public new void print()
+    {
+        Console.WriteLine("Ship:");
+        base.print();
+        Console.WriteLine("port: " + port);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        double[] cord = [12.3, 32.1, 1000];
+        Plane plane = new Plane(1000000, 890, 2010, 100, cord);
+        Ship ship= new Ship(1000000, 890, 2010, 100, "Samara", cord);
+        plane.print();
+        ship.print();
+    }
+}
+
