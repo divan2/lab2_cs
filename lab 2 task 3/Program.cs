2661e21b4e0f7698b91173040b66a4bc3c2c2f2c
Нина Павлова <samaraivan2005@yandex.ru>
2024-09-26 11:30:31 +0300

    lab 2 task 3

---
+++ lab 2 task 3/Program.cs
@@ -0,0 +1,64 @@
+﻿class DocumentWorker
+{
+    public virtual void OpenDocument()
+    {
+        Console.WriteLine("Документ открыт");
+    }
+
+    public virtual void EditDocument()
+    {
+        Console.WriteLine("Редактирование документа доступно в версии Pro");
+    }
+
+    public virtual void SaveDocument()
+    {
+        Console.WriteLine("Сохранение документа доступно в версии Pro");
+    }
+}
+
+class ProDocumentWorker : DocumentWorker
+{
+    public override void EditDocument()
+    {
+        Console.WriteLine("Документ отредактирован");
+    }
+
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Expert");
+    }
+}
+class ExpertDocumentWorker : ProDocumentWorker
+{
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в новом формате");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string key = "";
+        Console.WriteLine("Введите ключ доступа (pro или exp), либо нажмите Enter для бесплатной версии:");
+        key = Console.ReadLine();
+        DocumentWorker doc;
+        if (key == "pro")
+        {
+            doc = new ProDocumentWorker();
+        }
+        else if (key == "exp")
+        {
+            doc = new ExpertDocumentWorker();
+        }
+        else
+        {
+            doc = new DocumentWorker();
+        }
+
+        doc.OpenDocument();
+        doc.EditDocument();
+        doc.SaveDocument();
+    }
+}
